3acc6c92a901bf853853e1d4656aedc0f28cc913
Eduardo Santos <eduardo.santos.ct05@gmail.com>
2026-09-22 10:24:53 -0300

    Adiciona verificação de aluno ativo

--- modulo-04/cadastro-aluno/aluno.js
+++ modulo-04/cadastro-aluno/aluno.js
@@ -8,3 +8,9 @@ let aluno = {
 console.log(`Nome: ${aluno.nome}`)
 console.log(`Idade: ${aluno.idade}`)
 console.log(`Curso: ${aluno.curso}`)
+
+if (aluno.ativo) {
+    console.log('Situação: aluno(a) ativo')
+} else {
+    console.log('Situação: aluno(a) inativo')
+}
