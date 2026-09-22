3540c55be77187c81f88eba0bb7717b1a8ecf25b
Eduardo Santos <eduardo.santos.ct05@gmail.com>
2026-09-22 10:01:28 -0300

    Primeiro commit: adiciona ficha do produto

---
+++ modulo-04/ficha-produto/produto.js
@@ -0,0 +1,8 @@
+const produto = {
+    nome: "Fone de Ouvido",
+    preco: 89.9,
+    estoque: 15
+};
+console.log("Produto: " + produto.nome);
+console.log("Preço: R$ " + produto.preco);
+console.log("Estoque: " + produto.estoque + " unidades");
