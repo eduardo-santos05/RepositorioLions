cbe77e573bc06ae00c03fca37b283d950dee7a00
Eduardo Santos <eduardo.santos.ct05@gmail.com>
2026-09-22 11:11:08 -0300

    Adiciona cálculo do valor total em estoque

--- modulo-04/exibicao-ficha-produto/produto.js
+++ modulo-04/exibicao-ficha-produto/produto.js
@@ -7,7 +7,10 @@ let produto = {
 function exibirFicha(produto) {
     console.log(`Produto: ${produto.nome}`)
     console.log(`Preço: R$ ${produto.preco}`)
-    console.log(`Estoque: ${produto.estoque}`)
+    console.log(`Estoque: ${produto.estoque} unidades`)
+
+    let valorTotalEstoque = produto.preco * produto.estoque
+    console.log(`Valor total em estoque: R$ ${valorTotalEstoque}`)
 }
 
 exibirFicha(produto)
