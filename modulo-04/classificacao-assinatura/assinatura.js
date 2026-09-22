84f8a44b21147b5a9053590307b679d8870b0e80
Eduardo Santos <eduardo.santos.ct05@gmail.com>
2026-09-22 11:00:17 -0300

    Adiciona classificação por tempo de assinatura

--- modulo-04/classificacao-assinatura/assinatura.js
+++ modulo-04/classificacao-assinatura/assinatura.js
@@ -7,3 +7,11 @@ let assinatura = {
 console.log("Cliente: " + assinatura.cliente)
 console.log("Plano: " + assinatura.plano)
 console.log("Meses ativos: " + assinatura.mesesAtivos)
+
+if (assinatura.mesesAtivos < 6) {
+    console.log("Classificação: cliente novo")
+} else if (assinatura.mesesAtivos < 24) {
+    console.log("Classificação: cliente fiel")
+} else {
+    console.log("Classificação: cliente veterano")
+}
