308a2c9dda4389d074d377600183206839986e0c
Eduardo Santos <eduardo.santos.ct05@gmail.com>
2026-09-22 10:40:51 -0300

    Adiciona verificação de pagamento

--- modulo-04/status-pedido/pedido.js
+++ modulo-04/status-pedido/pedido.js
@@ -6,3 +6,9 @@ let pedido = {
 
 console.log('Cliente: ' + pedido.cliente)
 console.log('Valor do pedido: R$ ' + pedido.valor)
+
+if (pedido.pago) {
+    console.log('Status: pagamento confirmado')
+} else {
+    console.log('Status: aguardando pagamento')
+}
