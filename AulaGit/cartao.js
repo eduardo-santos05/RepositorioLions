64748fbb2db3299d13d55cab1d6847b00d3b1684
Eduardo Santos <eduardo.santos.ct05@gmail.com>
2026-09-22 09:01:06 -0300

    Adicionado o código de cartão.js

---
+++ AulaGit/cartao.js
@@ -0,0 +1,9 @@
+let nome = 'Eduardo'
+let idade = 21
+let cidade = 'Ponta Grossa'
+let curso = 'Engenharia de Software'
+
+console.log(`Nome: ${nome}`)
+console.log(`Idade: ${idade}`)
+console.log(`Cidade: ${cidade}`)
+console.log(`Curso: ${curso}`)
