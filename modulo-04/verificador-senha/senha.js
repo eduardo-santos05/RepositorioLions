436f4bdd4974822ed74d74838b5be69f561c13a6
Eduardo Santos <eduardo.santos.ct05@gmail.com>
2026-09-22 10:14:30 -0300

    Primeiro commit: adiciona verificador de senha

---
+++ modulo-04/verificador-senha/senha.js
@@ -0,0 +1,8 @@
+const senhaCorreta = 'lions2024'
+const senhaDigitada = 'lions2024'
+
+if (senhaDigitada === senhaCorreta) {
+    console.log('Acesso liberado.')
+} else {
+    console.log('Acesso negado.')
+}
