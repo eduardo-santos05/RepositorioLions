ab871eaac6218c6006855afb5367100e0e378d91
Eduardo Santos <eduardo.santos.ct05@gmail.com>
2026-09-22 10:50:22 -0300

    Revert "Primeiro commit: adiciona classificação de"
    This reverts commit d5ff0f2b59f6a13bedde7a7f3d27c38ee00bf44d.

--- modulo-04/classificacao-assinatura/assinatura.js
+++
@@ -1,9 +0,0 @@
-let assinatura = {
-    cliente: 'Beatriz Nunes',
-    plano: 'Premium',
-    mesesAtivos: 14
-}
-
-console.log("Cliente: " + assinatura.cliente)
-console.log("Plano: " + assinatura.plano)
-console.log("Meses ativos: " + assinatura.mesesAtivos)
