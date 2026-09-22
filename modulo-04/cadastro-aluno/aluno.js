f2d3d457665a2c7c6aed8287ea613fed9a95b628
Eduardo Santos <eduardo.santos.ct05@gmail.com>
2026-09-22 10:21:57 -0300

    Primeiro commit: adiciona cadastro de aluno

---
+++ modulo-04/cadastro-aluno/aluno.js
@@ -0,0 +1,10 @@
+let aluno = {
+    nome: 'Camila Rocha',
+    idade: 19,
+    curso: 'Trilha JavaScript',
+    ativo: true
+}
+
+console.log(`Nome: ${aluno.nome}`)
+console.log(`Idade: ${aluno.idade}`)
+console.log(`Curso: ${aluno.curso}`)
