f79f582ea64f402eb4d7bb84a3563e38090ce34f
Eduardo Santos <eduardo.santos.ct05@gmail.com>
2026-09-23 09:39:52 -0300

    Testando a branch nova

--- AulaGit/cartao.js
+++
@@ -1,9 +0,0 @@
-let nome = 'Paula'
-let idade = 21
-let cidade = 'Ponta Grossa'
-let curso = 'Engenharia de Software'
-
-console.log(`Nome: ${nome}`)
-console.log(`Idade: ${idade}`)
-console.log(`Cidade: ${cidade}`)
-console.log(`Curso: ${curso}`)
